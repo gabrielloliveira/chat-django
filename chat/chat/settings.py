08972771a4c557db8043bd916396b1f9429d71ad
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2020-03-20 19:38:30 -0300

    ending the project

--- chat/chat/settings.py
+++ chat/chat/settings.py
@@ -121,4 +121,13 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-ASGI_APPLICATION = "core.routing.application"
+ASGI_APPLICATION = 'chat.routing.application'
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
--- chat/core/routing.py
+++ chat/core/routing.py
@@ -1,5 +1,6 @@
-from channels.routing import ProtocolTypeRouter
+from django.urls import re_path
+from . import consumers
 
-application = ProtocolTypeRouter({
-    # (http->django views is added by default)
-})
+websocket_urlpatterns = [
+    re_path(r'ws/chat/$', consumers.ChatConsumer),
+]
