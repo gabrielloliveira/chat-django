58a396971acaf1b4e6e73245d2ad1a6fdccb56ed
Gabriell Oliveira <gabrielloliveira097@gmail.com>
2021-06-14 20:05:16 -0300

    corrigindo exception de organization

--- apps/core/signals.py
+++ apps/core/signals.py
@@ -39,7 +39,7 @@ def update_message(sender, instance, created, **kwargs):
     if created:
         type_ = "new_message"
     channel_layer = get_channel_layer()
-    group_name = f'organization_{instance.organization}'
+    group_name = f'organization_{instance.help_desk.organization}'
     async_to_sync(channel_layer.group_send)(
         group_name,
         {
